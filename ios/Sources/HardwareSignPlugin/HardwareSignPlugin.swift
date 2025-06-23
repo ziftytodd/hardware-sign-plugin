b9670e421426f3f6feafc86c9608673545037e7e
Todd Miller <todd@zifty.com>
2025-06-22 21:22:11 -0400

    Fix iOS recursive issue

--- ios/Sources/HardwareSignPlugin/HardwareSignPlugin.swift
+++ ios/Sources/HardwareSignPlugin/HardwareSignPlugin.swift
@@ -6,11 +6,28 @@ public class HardwareSignPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "HardwareSignPlugin"
     public let jsName = "HardwareSignPlugin"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "generateKey", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "sign", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getPublicKey", returnType: CAPPluginReturnPromise)
     ]
-    private let implementation = HardwareSignPlugin()
 
-    @objc func echo(_ call: CAPPluginCall) {
+    @objc func generateKey(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        print(value)
+        call.resolve([
+            "value": value
+        ])
+    }
+
+    @objc func sign(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        print(value)
+        call.resolve([
+            "value": value
+        ])
+    }
+
+    @objc func getPublicKey(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         print(value)
         call.resolve([
